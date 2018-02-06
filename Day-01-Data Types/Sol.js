44ea3e93e339d243f629ee2d969cb3cebdf2f617
pradeek p mohandas <pradeek.pm@gmail.com>
2018-02-07 00:57:46 +0530

    Create Sol.js

---
+++ Day-01-Data Types/Sol.js
@@ -0,0 +1,15 @@
+   // Declare second integer, double, and String variables.
+ var ii
+ var dd
+ var ss
+    // Read and save an integer, double, and String to your variables.
+    ii = parseInt(readLine());
+    dd = parseFloat(readLine());
+    ss = readLine();
+  // Print the sum of both integer variables on a new line.
+    console.log(i + ii);
+    // Print the sum of the double variables on a new line.
+    console.log((d + dd).toFixed(1));
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    console.log(s + ss);
