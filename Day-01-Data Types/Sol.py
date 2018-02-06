44f7b9371854e1a7b9dea65888a1528eeb09cb41
pradeek p mohandas <pradeek.pm@gmail.com>
2018-02-07 00:51:37 +0530

    Create Sol.py

---
+++ Day-01-Data Types/Sol.py
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+ii = int(input())
+dd = float(input())
+ss = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + ii)
+
+# Print the sum of the double variables on a new line.
+print(d + dd)
+
+print(s + ss)
