38040c8f6be021a0b9b17491dd87bf1c04ae7cde
pradeek p mohandas <pradeek.pm@gmail.com>
2018-04-21 16:13:58 +0530

    Create Sol.java

---
+++ Day-01-Data Types/Sol.java
@@ -0,0 +1,18 @@
+        /* Declare second integer, double, and String variables. */
+        int ii;
+        double dd;
+        String ss;
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        ii = Integer.parseInt(scan.nextLine());
+        dd = Double.parseDouble(scan.nextLine());
+        ss  = scan.nextLine();
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i+ii);
+        System.out.println(d+dd);
+        System.out.println(s+ss);
+
+
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
